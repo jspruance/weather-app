2c858d9568269534500d445dbaec84642ebadf5b
Jonathan Spruance <jgspruance@gmail.com>
2019-01-17 17:01:17 -0800

    Add event handler example

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class Greeting extends Component {
 
   state = {
-    username: "Jane",
-    isLoggedIn: false
+    username: "",
+    isLoggedIn: true,
+    count: 0
+  }
+
+  handleClick = () => {
+    this.setState((state, props) => ({
+      count: state.count + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.setState((state, props) => ({
+      username: "Bob"
+    }));
   }
 
   render() {
     if (this.state.isLoggedIn) {
-      return <h1>Hello {this.state.username}</h1>;
+      return (
+        <Fragment>
+          <h1>Hello {this.state.username}</h1>
+          <p>You have visited this site {this.state.count} times.</p>
+          <button onClick={this.handleClick}>Click me</button>
+        </Fragment>
+      );
     }else {
       return <p>Please sign up or log in.</p>
     }
+    
   }
 }
 
